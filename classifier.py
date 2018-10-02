44dbf39f1436b9ea98bbfc89158df65802fab17b
vnerhus <vegard@nerhus.no>
2018-10-02 14:52:45 +0200

    Initial commit

---
+++ classifier.py
@@ -0,0 +1,323 @@
+from urllib.request import urlretrieve
+from os.path import isfile, isdir
+from tqdm import tqdm
+import tensorflow as tf
+import tarfile
+import helper
+import numpy as np
+import pickle
+import helper
+import random
+
+cifar10_dataset_folder_path = 'cifar-10-batches-py'
+
+# Use Floyd's cifar-10 dataset if present
+floyd_cifar10_location = '/input/cifar-10/python.tar.gz'
+if isfile(floyd_cifar10_location):
+    tar_gz_path = floyd_cifar10_location
+else:
+    tar_gz_path = 'cifar-10-python.tar.gz'
+
+class DLProgress(tqdm):
+    last_block = 0
+
+    def hook(self, block_num=1, block_size=1, total_size=None):
+        self.total = total_size
+        self.update((block_num - self.last_block) * block_size)
+        self.last_block = block_num
+
+if not isfile(tar_gz_path):
+    with DLProgress(unit='B', unit_scale=True, miniters=1, desc='CIFAR-10 Dataset') as pbar:
+        urlretrieve(
+            'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz',
+            tar_gz_path,
+            pbar.hook)
+
+if not isdir(cifar10_dataset_folder_path):
+    with tarfile.open(tar_gz_path) as tar:
+        tar.extractall()
+        tar.close()
+
+# Explore the dataset
+batch_id = 3
+sample_id = 400
+helper.display_stats(cifar10_dataset_folder_path, batch_id, sample_id)
+
+def normalize(x):
+    """
+    Normalize a list of sample image data in the range of 0 to 1
+    : x: List of image data.  The image shape is (32, 32, 3)
+    : return: Numpy array of normalize data
+    """
+
+    return np.array((x - np.min(x)) / (np.max(x) - np.min(x)))
+
+def one_hot_encode(x):
+    """
+    One hot encode a list of sample labels. Return a one-hot encoded vector for each label.
+    : x: List of sample Labels
+    : return: Numpy array of one-hot encoded labels
+    """
+    nb_classes = 10
+    targets = np.array(x).reshape(-1)
+    one_hot_targets = np.eye(nb_classes)[targets]
+
+    return one_hot_targets
+
+# Preprocess Training, Validation, and Testing Data
+helper.preprocess_and_save_data(cifar10_dataset_folder_path, normalize, one_hot_encode)
+
+# Load the Preprocessed Validation data
+valid_features, valid_labels = pickle.load(open('preprocess_validation.p', mode='rb'))
+
+def neural_net_image_input(image_shape):
+    """
+    Return a Tensor for a batch of image input
+    : image_shape: Shape of the images
+    : return: Tensor for image input.
+    """
+    x = tf.placeholder(tf.float32, shape=[None, image_shape[0], image_shape[1], image_shape[2]], name='x')
+    return x
+
+
+def neural_net_label_input(n_classes):
+    """
+    Return a Tensor for a batch of label input
+    : n_classes: Number of classes
+    : return: Tensor for label input.
+    """
+    y = tf.placeholder(tf.float32, shape=[None, n_classes], name='y')
+    return y
+
+
+def neural_net_keep_prob_input():
+    """
+    Return a Tensor for keep probability
+    : return: Tensor for keep probability.
+    """
+    keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+    return keep_prob
+
+
+def conv2d_maxpool(x_tensor, conv_num_outputs, conv_ksize, conv_strides, pool_ksize, pool_strides):
+    """
+    Apply convolution then max pooling to x_tensor
+    :param x_tensor: TensorFlow Tensor
+    :param conv_num_outputs: Number of outputs for the convolutional layer
+    :param conv_ksize: kernal size 2-D Tuple for the convolutional layer
+    :param conv_strides: Stride 2-D Tuple for convolution
+    :param pool_ksize: kernal size 2-D Tuple for pool
+    :param pool_strides: Stride 2-D Tuple for pool
+    : return: A tensor that represents convolution and max pooling of x_tensor
+    """
+    depth = x_tensor.get_shape().as_list()[3]
+
+    weight = tf.Variable(tf.truncated_normal([conv_ksize[0], conv_ksize[1], depth, conv_num_outputs], stddev=0.1))
+    bias = tf.Variable(tf.truncated_normal([conv_num_outputs], stddev=0.1))
+
+    conv_layer = tf.nn.conv2d(x_tensor, weight, [1, conv_strides[0], conv_strides[1], 1], padding='SAME')
+    conv_layer = tf.nn.bias_add(conv_layer, bias)
+    conv_layer = tf.nn.relu(conv_layer)
+    conv_layer = tf.nn.max_pool(conv_layer,
+                                [1, pool_ksize[0], pool_ksize[1], 1],
+                                [1, pool_strides[0], pool_strides[1], 1],
+                                padding='SAME')
+    return conv_layer
+
+def flatten(x_tensor):
+    """
+    Flatten x_tensor to (Batch Size, Flattened Image Size)
+    : x_tensor: A tensor of size (Batch Size, ...), where ... are the image dimensions.
+    : return: A tensor of size (Batch Size, Flattened Image Size).
+    """
+    flat_shape = np.prod([x for x in x_tensor.get_shape()[1:]])
+    x_flat = tf.reshape(x_tensor, [-1, flat_shape.value])
+    return x_flat
+
+def fully_conn(x_tensor, num_outputs):
+    """
+    Apply a fully connected layer to x_tensor using weight and bias
+    : x_tensor: A 2-D tensor where the first dimension is batch size.
+    : num_outputs: The number of output that the new tensor should be.
+    : return: A 2-D tensor where the second dimension is num_outputs.
+    """
+    weight = tf.Variable(tf.truncated_normal([x_tensor.get_shape().as_list()[1], num_outputs], stddev=0.1))
+    bias = tf.Variable(tf.truncated_normal([num_outputs], stddev=0.1))
+
+    fclayer = tf.add(tf.matmul(x_tensor, weight), bias)
+    fclayer = tf.nn.relu(fclayer)
+
+    return fclayer
+
+def output(x_tensor, num_outputs):
+    """
+    Apply a output layer to x_tensor using weight and bias
+    : x_tensor: A 2-D tensor where the first dimension is batch size.
+    : num_outputs: The number of output that the new tensor should be.
+    : return: A 2-D tensor where the second dimension is num_outputs.
+    """
+    weight = tf.Variable(tf.truncated_normal([x_tensor.get_shape().as_list()[1], num_outputs], stddev=0.1))
+    bias = tf.Variable(tf.truncated_normal([num_outputs], stddev=0.1))
+
+    output = tf.add(tf.matmul(x_tensor, weight), bias)
+    return output
+
+def conv_net(x, keep_prob):
+    """
+    Create a convolutional neural network model
+    : x: Placeholder tensor that holds image data.
+    : keep_prob: Placeholder tensor that hold dropout keep probability.
+    : return: Tensor that represents logits
+    """
+    conv1 = conv2d_maxpool(x, 32, [2, 2], [2, 2], [2, 2], [2, 2])
+    conv2 = conv2d_maxpool(conv1, 64, [2, 2], [2, 2], [1, 1], [1, 1])
+    conv3 = conv2d_maxpool(conv2, 128, [4, 4], [2, 2], [1, 1], [1, 1])
+
+    flat1 = flatten(conv3)
+
+    fc1 = fully_conn(flat1, 1024)
+    fc1 = tf.nn.dropout(fc1, keep_prob)
+    fc2 = fully_conn(fc1, 256)
+    fc2 = tf.nn.dropout(fc2, keep_prob)
+    fc3 = fully_conn(fc2, 64)
+    fc3 = tf.nn.dropout(fc3, keep_prob)
+
+    out = fully_conn(fc3, 10)
+    return out
+
+
+# Remove previous weights, bias, inputs, etc..
+tf.reset_default_graph()
+
+# Inputs
+x = neural_net_image_input((32, 32, 3))
+y = neural_net_label_input(10)
+keep_prob = neural_net_keep_prob_input()
+
+# Model
+logits = conv_net(x, keep_prob)
+
+# Name logits Tensor, so that is can be loaded from disk after training
+logits = tf.identity(logits, name='logits')
+
+# Loss and Optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
+optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+# Accuracy
+correct_pred = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32), name='accuracy')
+
+def train_neural_network(session, optimizer, keep_probability, feature_batch, label_batch):
+    """
+    Optimize the session on a batch of images and labels
+    : session: Current TensorFlow session
+    : optimizer: TensorFlow optimizer function
+    : keep_probability: keep probability
+    : feature_batch: Batch of Numpy image data
+    : label_batch: Batch of Numpy label data
+    """
+    session.run(optimizer, feed_dict={x: feature_batch, y: label_batch, keep_prob: keep_probability})
+    pass
+
+def print_stats(session, feature_batch, label_batch, cost, accuracy):
+    """
+    Print information about loss and validation accuracy
+    : session: Current TensorFlow session
+    : feature_batch: Batch of Numpy image data
+    : label_batch: Batch of Numpy label data
+    : cost: TensorFlow cost function
+    : accuracy: TensorFlow accuracy function
+    """
+    loss = sess.run(cost, feed_dict={
+        x: feature_batch,
+        y: label_batch,
+        keep_prob: 1.})
+    test_acc = sess.run(accuracy, feed_dict={
+        x: valid_features,
+        y: valid_labels,
+        keep_prob: 1.})
+
+    print('Loss: {} Testing Accuracy: {}'.format(loss, test_acc))
+
+
+
+### TRAIN MODEL ###
+epochs = 10
+batch_size = 256
+keep_probability = 0.75
+
+save_model_path = './image_classification'
+
+print('Training...')
+with tf.Session() as sess:
+    # Initializing the variables
+    sess.run(tf.initialize_all_variables())
+
+    # Training cycle
+    for epoch in range(epochs):
+        # Loop over all batches
+        n_batches = 5
+        for batch_i in range(1, n_batches + 1):
+            for batch_features, batch_labels in helper.load_preprocess_training_batch(batch_i, batch_size):
+                train_neural_network(sess, optimizer, keep_probability, batch_features, batch_labels)
+            print('Epoch {:>2}, CIFAR-10 Batch {}:  '.format(epoch + 1, batch_i), end='')
+            print_stats(sess, batch_features, batch_labels, cost, accuracy)
+
+    # Save Model
+    saver = tf.train.Saver()
+    save_path = saver.save(sess, save_model_path)
+
+### TEST MODEL ###
+# Set batch size if not already set
+try:
+    if batch_size:
+        pass
+except NameError:
+    batch_size = 64
+
+save_model_path = './image_classification'
+n_samples = 4
+top_n_predictions = 3
+
+def test_model():
+    """
+    Test the saved model against the test dataset
+    """
+
+    test_features, test_labels = pickle.load(open('preprocess_test.p', mode='rb'))
+    loaded_graph = tf.Graph()
+
+    with tf.Session(graph=loaded_graph) as sess:
+        # Load model
+        loader = tf.train.import_meta_graph(save_model_path + '.meta')
+        loader.restore(sess, save_model_path)
+
+        # Get Tensors from loaded model
+        loaded_x = loaded_graph.get_tensor_by_name('x:0')
+        loaded_y = loaded_graph.get_tensor_by_name('y:0')
+        loaded_keep_prob = loaded_graph.get_tensor_by_name('keep_prob:0')
+        loaded_logits = loaded_graph.get_tensor_by_name('logits:0')
+        loaded_acc = loaded_graph.get_tensor_by_name('accuracy:0')
+
+        # Get accuracy in batches for memory limitations
+        test_batch_acc_total = 0
+        test_batch_count = 0
+
+        for test_feature_batch, test_label_batch in helper.batch_features_labels(test_features, test_labels, batch_size):
+            test_batch_acc_total += sess.run(
+                loaded_acc,
+                feed_dict={loaded_x: test_feature_batch, loaded_y: test_label_batch, loaded_keep_prob: 1.0})
+            test_batch_count += 1
+
+        print('Testing Accuracy: {}\n'.format(test_batch_acc_total/test_batch_count))
+
+        # Print Random Samples
+        random_test_features, random_test_labels = tuple(zip(*random.sample(list(zip(test_features, test_labels)), n_samples)))
+        random_test_predictions = sess.run(
+            tf.nn.top_k(tf.nn.softmax(loaded_logits), top_n_predictions),
+            feed_dict={loaded_x: random_test_features, loaded_y: random_test_labels, loaded_keep_prob: 1.0})
+        helper.display_image_predictions(random_test_features, random_test_labels, random_test_predictions)
+
+
+test_model()
